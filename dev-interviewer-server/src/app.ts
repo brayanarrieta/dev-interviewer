2ccc6303c213d5dcb0eb52a9d8c41bea5a9b8f5a
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2021-06-09 13:02:24 -0600

    chore: add eslint linter to the server
    Add eslint linter to the server

--- dev-interviewer-server/src/app.ts
+++ dev-interviewer-server/src/app.ts
@@ -3,11 +3,11 @@ import express from 'express';
 const app = express();
 const PORT = 8000;
 
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
-} );
-
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
