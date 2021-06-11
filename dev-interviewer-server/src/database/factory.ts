29a79bc659b78cb2302193bd4ed7510797bbfb5e
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2021-06-11 10:06:26 -0600

    feat: add new factory methods insertRecord, updateRecord, deleteRecord related to harperive
    Add new factory methods insertRecord, updateRecord, deleteRecord related to harperive

--- dev-interviewer-server/src/database/factory.ts
+++ dev-interviewer-server/src/database/factory.ts
@@ -2,11 +2,11 @@ import { HARPER_SCHEMA } from '../config';
 import { ServiceError } from '../helpers/errors';
 import harperiveClient from './harperive';
 
-type PrepareRawQueryOptions = {
+type Options = {
     schema?: string
 }
 
-const prepareRawQuery = (query: string, options?: PrepareRawQueryOptions): string => {
+const prepareRawQuery = (query: string, options?: Options): string => {
   const schema = options?.schema || HARPER_SCHEMA;
 
   if (!schema) {
@@ -21,8 +21,66 @@ const prepareRawQuery = (query: string, options?: PrepareRawQueryOptions): strin
   return prepared;
 };
 
-export const executeRawQuery = async (query: string, options?: PrepareRawQueryOptions) => {
+export const executeRawQuery = async (query: string, options?: Options) => {
   const rawQuery = prepareRawQuery(query, options);
   const result = await harperiveClient.query(rawQuery);
   return result;
+};
+
+export const insertRecord = async (tableName: string, record: Object, options?: Options) => {
+  const schema = options?.schema || HARPER_SCHEMA;
+
+  if (!schema) {
+    throw new ServiceError({
+      token: 'NO_SCHEMA_PROVIDED',
+    });
+  }
+
+  const inserted = await harperiveClient.insert({
+    table: tableName,
+    records: [
+      record,
+    ],
+    schema,
+  });
+
+  return inserted;
+};
+
+export const updateRecord = async (tableName: string, record: Object, options?: Options) => {
+  const schema = options?.schema || HARPER_SCHEMA;
+
+  if (!schema) {
+    throw new ServiceError({
+      token: 'NO_SCHEMA_PROVIDED',
+    });
+  }
+
+  const updated = await harperiveClient.update({
+    table: tableName,
+    records: [
+      record,
+    ],
+    schema,
+  });
+
+  return updated;
+};
+
+export const deleteRecord = async (tableName: string, recordId: string, options?: Options) => {
+  const schema = options?.schema || HARPER_SCHEMA;
+
+  if (!schema) {
+    throw new ServiceError({
+      token: 'NO_SCHEMA_PROVIDED',
+    });
+  }
+
+  const deleted = await harperiveClient.delete({
+    schema,
+    table: tableName,
+    hashValues: [recordId],
+  });
+
+  return deleted;
 };
