98d8b6dcb3d03c959caefe23c2f1e1d8f1be1337
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2021-06-15 14:54:46 -0600

    feat(backend): refactor structure returned from the get tags endpoint

--- dev-interviewer-server/src/controllers/tags.controller.ts
+++ dev-interviewer-server/src/controllers/tags.controller.ts
@@ -3,5 +3,5 @@ import { getAllTags } from '../services/tags.service';
 
 export const getTags = async (req: Request, res: Response) => {
   const tags = await getAllTags();
-  res.json(tags);
+  res.json({ tags });
 };
